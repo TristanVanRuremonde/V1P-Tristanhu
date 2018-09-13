bcb0992b280a491563efdffc3701a3b875902580
TristanVanRuremonde <tristan.vanruremonde@student.hu.nl>
2018-09-13 11:23:27 +0200

    TristanVanRuremonde tristan.vanruremonde@student.hu.nl 4.3 opdracht (als test)

---
+++ 4.3.py
@@ -0,0 +1,5 @@
+verdien = input("wat verdien je per uur?")
+gewerkt = input("Hoeveel uur heb je gewerkt?")
+wiskunde = int(verdien) * int(gewerkt)
+zin = str(verdien) + " uur werken levert " + str(wiskunde) + " euro op"
+print(zin)
