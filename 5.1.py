2ee12c87b03075b693d08eb2d8c081c449237876
TristanVanRuremonde <tristan.vanruremonde@student.hu.nl>
2018-09-19 16:44:16 +0200

    5.1

---
+++ 5.1.py
@@ -0,0 +1,6 @@
+score = (input("Wat was je score?: "))
+print("Je score is " + str(score))
+if int(score) > 15:
+    print("Gefeliciteerd, je bent geslaagd")
+else:
+    print("Sorry, probeer het opnieuw")
