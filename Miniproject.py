39a0a783961296f245579fd8fe88ada7354fe598
TristanVanRuremonde <tristan.vanruremonde@student.hu.nl>
2018-10-11 10:14:26 +0200

    PROG miniproject python code

--- Miniproject.py
+++ Miniproject.py
@@ -13,7 +13,7 @@ def login_menu():
         if loginkeuze == 1:
             inloggen()
         elif loginkeuze == 2:
-            gebruikersInformatie()
+            registreren()
 
         else:
             print('Error login')
@@ -37,20 +37,19 @@ def ingelogd():
     try:
         loginkeuze = int(input("Wat wil je doen?: \n 1. Fiets stallen \n 2. fiets ophalen \n 3. informatie opvragen \n Keuze: "))
         if loginkeuze == 1:
-            # extra keer inloggen verwijderen.
-            fietsStallen()
+            openStalling()
         elif loginkeuze == 2:
-            gebruikersInformatie()
+            fietsOphalen()
         elif loginkeuze == 3:
-            print('3')
+            algemeneInformatie()
         else:
             print('Error login')
     except ValueError:
         print("Graag een cijfer invullen")
 
 
+
 def uniqueNumber():
-    print('UniqueNumber')
     'genereert een uniek getal voor de gebruiker'
     uniekNummer = ''
     while True:
@@ -68,10 +67,9 @@ def uniqueNumber():
     return uniekNummer
 
 
-def gebruikersInformatie():
-    print('Gebruikersinformatie')
+def registreren():
     'vraagt gebruiker voor zijn/haar naam'
-    voornaamUser = str(input(('Voornaam: ')))
+    voornaamUser = str(input('Voornaam: '))
     achternaamUser = str(input('Achternaam: '))
     tussenvoegselUser = str(input('Tussenvoegsel [laat leeg indien je er geen hebt...]: '))
     while True:
@@ -85,7 +83,7 @@ def gebruikersInformatie():
                 noOfIntsInPass += 1
             except ValueError:
                 pass
-        if len(wachtwoord) > 6 and noOfIntsInPass >= 2:
+        if len(wachtwoord) >= 6 and noOfIntsInPass >= 2:
             break
         else:
             print('Incorrect wachtwoord. Probeer het opnieuw')
@@ -97,40 +95,63 @@ def gebruikersInformatie():
         naamUser = voornaamUser + ' ' + tussenvoegselUser + ' ' + achternaamUser
         # callt de functie uniqueNumber voor een random string als uniek nummer
     gebruikersNummer = uniqueNumber()
-    with open('Gebruikersbestand.csv', 'a', newline='\n') as CSV:
-
-        appender = csv.append(CSV, delimiter=';')
-        appender.writerow((gebruikersNummer, naamUser, wachtwoord, ''))
-    print(gebruikersNummer)
-
-
-def fietsStallen():
-    print('fietsenstallen')
-    correcteGegevens = False
-    huidigeTijd = datetime.datetime.now()
-    opgegevenGebruikersnaam = str(input('Geef je unieke code: '))
-    opgegevenWachtwoord = str(input('Voer je wachtwoord in: '))
-    with open('Gebruikersbestand.csv', 'r') as CSV:
-        for o in CSV.readlines():
-            print(o.split(';')[0])
-            print(o.split(';')[2])
-            wachtwoord = o.split(';')[2]
-            if opgegevenGebruikersnaam == o.split(';')[0]:
-                if wachtwoord == opgegevenWachtwoord:
-
-
-                    #extra lijn toevoegen  dat zegt gestald = true of false
-                    #Waar gestald?
-
-                    correcteGegevens = True
-            else:
-                print('Iets ging fout. Probeer het opnieuw')
-    with open('Stallingsbestand', 'r+', newline='') as CSV:
+    with open('Gebruikersbestand.csv', 'a', newline='') as CSV:
         writer = csv.writer(CSV, delimiter=';')
 
-        if correcteGegevens == True:
-            writer.writerow((opgegevenGebruikersnaam, huidigeTijd))
+        writer.writerow((gebruikersNummer, naamUser, wachtwoord, ''))
+def login():
+    lstGebruikersGegevens = []
+    with open('Gebruikersbestand.csv', 'r') as CSV:
+            opgegevenGebruikersnaam = str(input('Geef je unieke code: '))
+            opgegevenWachtwoord = str(input('Voer je wachtwoord in: '))
+            for o in CSV.readlines():
+                wachtwoord = o.split(';')[2]
+                if opgegevenGebruikersnaam == o.split(';')[0]:
+                    if wachtwoord == opgegevenWachtwoord:
+                        print('Ingelogd')
+                        lstGebruikersGegevens = o
+                        break
+                else:
+                    print('Iets ging fout. Probeer het opnieuw')
+                    break
+    return lstGebruikersGegevens
+
+def openStalling(naam):
+    huidigeTijd = datetime.datetime.now()
+    with open('Stallingsbestand.csv', 'a', newline='') as CSV:
+        writer = csv.writer(CSV, delimiter=';')
+        writer.writerow((naam, huidigeTijd))
 
 
+def algemeneInformatie():
+    huidigeTijd = datetime.datetime.now()
+    counterUsersLastHour = 0
+    with open('Gebruikersbestand.csv', 'r', newline='') as CSV:
+        hoeveelheidGebruikers = len(CSV.readlines())
+    with open('Stallingsbestand.csv', 'r', newline='') as CSV:
+        for o in CSV.readlines():
+            lijn = o.split(';')[1]
+            huidigUur = str(huidigeTijd).split(':')[0]
+            if huidigUur == lijn.split(':')[0]:
+                counterUsersLastHour += 1
+    print('Aantal gebruikers van dit programma: ', hoeveelheidGebruikers)
+    print('Aantal gebruikers ingelogd in het laatste uur: ', counterUsersLastHour)
+
+
+def persoonlijkeInformatie():
+    lstGebruikersGegevens = login()
+    huidigeTijd = datetime.datetime.now()
+    print('Welkom ', lstGebruikersGegevens.split(';')[1])
+    print('Het is nu: ', huidigeTijd)
+def fietsOphalen():
+    correcteGegevens = False
+    lstGebruikersGegevens = login()
+    with open('Gebruikersbestand.csv', 'r') as CSV:
+        for o in CSV.readlines():
+            while correcteGegevens == False:
+                if o.split(';')[0] == lstGebruikersGegevens.split(';')[0]:
+                    print('Verhaaltje over ophalen van een fiets, hoezee.')
+                    correcteGegevens = True
+                    break
 
 login_menu()
